c3a60e6d7db8e801d71097940840280e8b526fcc
0P <j_unruh@hotmail.com>
2015-11-10 08:40:14 -0700

    upgrade om.next to alpha 18

--- project.clj
+++ project.clj
@@ -6,6 +6,6 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.145"]
                  [org.clojure/test.check "0.8.2"]
-                 [org.omcljs/om "1.0.0-alpha13"]
+                 [org.omcljs/om "1.0.0-alpha18"]
                  [figwheel-sidecar "0.4.0" :scope "provided"]
                  [com.andrewmcveigh/cljs-time "0.3.14"]])
